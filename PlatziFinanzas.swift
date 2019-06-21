23eec1e196bea6dd29d665cbc682f79b5648bafe
iMrLag <emo30000@gmail.com>
2019-06-21 13:30:46 -0500

    Iniciando Proyecto

---
+++ PlatziFinanzas.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+var accountTotal: Float = 1_000_000.0
+
+let name: String = "Andres"
+let lastName: String = "silva"
+
+let fullName = "\(name) \(lastName)"
+
+accountTotal +=  100_000
+
+print("accountTotal")
