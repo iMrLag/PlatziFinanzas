ba8006e2aa9bd788043d61d498e3c622fe35ff1a
iMrLag <emo30000@gmail.com>
2019-06-21 14:11:32 -0500

    manejo de transacciónes

--- PlatziFinanzas.swift
+++ PlatziFinanzas.swift
@@ -17,3 +17,34 @@ var isActive = !fullName.isEmpty
 print("isActive")
 
 print("Hola me llamo \(name) \(lastName) y tengo \(accountTotal)" en mi cuenta de banco.)
+
+var transactions: [Float]= [20, 10, 100.0]
+
+print("transaction")
+
+transactions.count
+
+// Es más eficiente que el de arriba
+transactions.isEmpty
+
+// Sirve para agregar elementos a una variable
+transactions.append()
+
+print("transactions")
+
+// Sirve para ver la primera transación
+transactions.first
+
+// Sirve para ver la última transación
+transactions.last
+
+// Elimina el ultimo elemento
+transactions.removeLast()
+
+print(transactions)
+
+transactions.removeFirst()
+transactions.remiveAll()
+
+transactions.min()
+transactions.max()
